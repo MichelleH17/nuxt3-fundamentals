15c832023e68fbd7873860675b22682f3e964b19
Michaela Havlíková <Michelle.h@seznam.cz>
2024-11-02 21:59:51 +0100

    Create new pages movies and teams

---
+++ pages/movies/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const query = ref("")
+
+function search() {
+  $fetch(`http://www.omdbapi.com/?apikey=4203574e&s=${query.value}`)
+}
+</script>
+
+<template>
+  <form @submit.prevent="search">
+    <input type="text" v-model="query">
+    <button>Search</button>
+  </form>
+</template>
+
+<style scoped></style>
