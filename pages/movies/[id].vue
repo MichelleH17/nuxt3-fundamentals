57521b2a4f40918e7c7458010bcb7929b6a88b35
Michaela Havlíková <Michelle.h@seznam.cz>
2024-11-03 00:28:53 +0100

    Use pick options to minimize payload size

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -9,5 +9,15 @@ const route = useRoute()
 
 const { data } = await useAsyncData(`/movies/${route.params.id}`, () => {
   return $fetch(`http://www.omdbapi.com/?apikey=4203574e&i=${route.params.id}`)
+},
+{
+  pick: ["Plot", "Title"],
+  /* nebo
+  transform(data) {
+    return {
+      Plot: data.Plot,
+      Title: data.Title,
+    }
+  } */
 })
 </script>
