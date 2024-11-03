e8a74df70ee374d72a3b57eb107ed0c1e7df5861
Michaela Havlíková <Michelle.h@seznam.cz>
2024-11-03 10:42:31 +0100

    Handle useFetch 404 error

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -7,10 +7,14 @@
 <script lang="ts" setup>
 const route = useRoute()
 // shortshand for $fetch within useAsyncData
-const { data } = useFetch(`http://www.omdbapi.com/?apikey=4203574e&i=${route.params.id}`, {
-  pick: ["Plot", "Title"],
+const { data } = await useFetch(`http://www.omdbapi.com/?apikey=4203574e&i=${route.params.id}`, {
+  pick: ["Plot", "Title", "Error"],
   key: `/movies/${route.params.id}`,
 })
+
+if (data.value.Error === "Incorrect IMDb ID.") {
+      showError({ statusCode: 404, statusMessage: "Page not found" })
+    }  
 /* const { data } = await useAsyncData(`/movies/${route.params.id}`, () => {
   return $fetch(`http://www.omdbapi.com/?apikey=4203574e&i=${route.params.id}`)
 },
